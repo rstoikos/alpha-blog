de82ec8adda4e1735a86a088f21a29800621adb4
rstoikos <rstoikos@hotmail.com>
2016-10-17 21:43:33 +0300

    user validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,6 +19,7 @@ end
 def create
   #render plain: params[:article].inspect
   @article = Article.new(article_params)
+  @article.user = User.first
   if @article.save
     flash[:success] = "Article was successfully created"
     redirect_to article_path(@article)
